3257bc421e9f0cba90a08348a3eee3e90147780f
Cristiano Manhães de Oliveira <cristianomoliveira@gmail.com>
2021-06-21 22:57:19 -0300

    FEAT - add multiplicao, divisao and subtracao

--- hello_django/core/views.py
+++ hello_django/core/views.py
@@ -5,3 +5,19 @@ from django.shortcuts import render, HttpResponse
 def hello(request, nome, idade):
     print("Hello")
     return HttpResponse('<h1>Oi {} de {} anos</h1>'.format(nome, idade))
+
+def soma(request, n1, n2):
+    soma = n1 + n2
+    return HttpResponse(soma)
+
+def multiplicacao(request, n1, n2):
+    mult = n1 * n2
+    return HttpResponse(mult)
+
+def divisao(request, n1, n2):
+    divisao = n1 / n2
+    return HttpResponse(divisao)
+
+def subtracao(request, n1, n2):
+    return HttpResponse(n1 - n2)
+
